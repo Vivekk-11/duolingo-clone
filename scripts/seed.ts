99f2f0b507b673547c1d02f3f811c48fe5caa914
vivek <chimnanivivek14@gmail.com>
2024-03-29 18:25:41 +0530

    database seed command

---
+++ scripts/seed.ts
@@ -0,0 +1,47 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+import * as schema from "../db/schema";
+
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle(sql, { schema });
+
+const main = async () => {
+  try {
+    console.log("Seeding the database!");
+
+    await db.delete(schema.courses);
+    await db.delete(schema.userProgress);
+
+    await db.insert(schema.courses).values([
+      {
+        id: 1,
+        title: "Spanish",
+        imageSrc: "/es.svg",
+      },
+      {
+        id: 2,
+        title: "Japanese",
+        imageSrc: "/jp.svg",
+      },
+      {
+        id: 3,
+        title: "Italian",
+        imageSrc: "/it.svg",
+      },
+      {
+        id: 4,
+        title: "French",
+        imageSrc: "/fr.svg",
+      },
+    ]);
+
+    console.log("Seeding finished!");
+  } catch (error) {
+    console.log("Error while seeding the database:-", error);
+    throw new Error("Failed to seed the database!");
+  }
+};
+
+main();
